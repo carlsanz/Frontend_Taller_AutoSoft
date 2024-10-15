c18bf9f072519d0645adc24e2b93f12909e0e9e0
kennybenitez <kennybenitez2013@gmail.com>
2024-10-15 10:15:28 -0600

    validacion en la parte del formulario de registro a mecanico

--- src/AgregarUsuarioForm.js
+++ src/AgregarUsuarioForm.js
@@ -4,12 +4,42 @@ import './AgregarUsuarios.css';
 
 const AgregarUsuario = () => {
     const [nombre, setNombre] = useState('');
+    const [nombreError, setnombreError] = useState('');
     const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
     const [password, setPassword] = useState('');
     const [firstLogin, setFirstLogin] = useState(true); // Campo para primer inicio de sesión
 
+    //Expresiones regulares 
+    //validacion nombre
+    const validateName = (value)=>{
+        const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ' ]{2,50}$/;
+
+        if (!nameRegex.test(nombre)){
+            setnombreError('El nombre es demasiado corto');
+            }else{
+                setnombreError('');
+            }
+        };
+    
+
+    //validacion email
+    const validateEmail = (value) =>{
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)){
+          setEmailError('Por favor introduce un correo valido');
+        }else{
+          setEmailError('');
+        }
+      };
+
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (nombreError | emailError) return;
+
         try {
             const response = await axios.post('http://localhost:5000/usuarios/agregar', { 
                 nombre, 
@@ -36,18 +66,24 @@ const AgregarUsuario = () => {
                     className="form-input"
                     type="text"
                     value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
+                    onChange={(e) => {setNombre(e.target.value)
+                                validateName(e.target.value);}}
+                    onBlur={(e)=> validateName(e.target.value)}            
                     placeholder="Nombre"
                     required
                 />
+                {nombreError && <p id="MsjError" className="error.message">{nombreError}</p>}
                 <input
                     className="form-input"
                     type="email"
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => {setEmail(e.target.value)
+                        validateEmail(e.target.value);}}
+                    onBlur={(e)=> validateEmail(e.target.value)}
                     placeholder="Email"
                     required
                 />
+                {emailError && <p id="MsjError" className="error.message">{emailError}</p>}
                 <input
                     className="form-input"
                     type="password"
